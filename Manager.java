65ade60b3139a67d7849ce04e8c132ccf10a6dcb
Sagarduhan1515 <633sagarcse2@gmail.com>
2022-12-09 20:18:41 +0530

    second commit

---
+++ Manager.java
@@ -0,0 +1,101 @@
+import java.sql.SQLException;
+import java.util.Scanner;
+
+
+import static java.lang.System.out;
+public class Manager {
+    private final String token;
+
+    public Manager(String token)
+    {
+        if(!DbHandler.validator(token))
+        {
+            System.out.println("Invalid token received");
+        }
+        this.token = token;
+    }
+
+    public void viewMenu() throws SQLException {
+        DbHandler.db_read_menu();
+        this.showManagementConsole();
+    }
+
+    public void addItemToMenu(String itemName,float itemPrice) throws SQLException {
+       DbHandler.insert_new_item(token,itemName,itemPrice);
+       this.showManagementConsole();
+    }
+
+    public void deleteItemFromMenu(String itemName) throws SQLException {
+        DbHandler.delete_from_menu(token,itemName);
+        this.showManagementConsole();
+    }
+
+    public void deleteItemFromMenu(int itemSNo) throws SQLException {
+        DbHandler.delete_from_menu(token,itemSNo);
+        this.showManagementConsole();
+    }
+
+    public void viewFeedback() throws SQLException {
+        DbHandler.view_feedback();
+        this.showManagementConsole();
+    }
+
+    public void feedbackAnalysis() throws SQLException {
+        if(!DbHandler.validator(token))
+        {
+            out.println("Invalid token");
+            return;
+        }
+
+        DbHandler.view_feedback_analysis();
+        this.showManagementConsole();
+    }
+    public void  showManagementConsole() throws SQLException {
+
+        Scanner scanner = new Scanner(System.in);
+        out.println("Press Enter...");
+        scanner.nextLine();
+        String choice;
+        out.println("----------------------Management Console----------------------");
+        out.println("Menu:\n1. View Menu\n2. Add Item to menu\n3. Delete Item from menu\n4. View customer feedback\n5. Feedback analysis\n6. Main menu");
+        choice = scanner.nextLine();
+
+        switch(choice)
+        {
+            case "1":
+                this.viewMenu();
+                break;
+            case "2":
+                out.println("Enter item name: ");
+                String itemName = scanner.nextLine();
+                out.println("Enter item price: ");
+                float price = Float.parseFloat(scanner.nextLine());
+                this.addItemToMenu(itemName,price);
+                break;
+            case "3":
+                out.println("Enter item no (sno) to be deleted: ");
+                int itemno = Integer.parseInt(scanner.nextLine());
+                this.deleteItemFromMenu(itemno);
+                break;
+            case "4":
+                this.viewFeedback();
+            case "5":
+                feedbackAnalysis();
+                break;
+            case "6":
+                Main.menu();
+                break;
+            default:
+                this.showManagementConsole();
+
+        }
+
+
+
+    }
+
+
+
+
+
+}
